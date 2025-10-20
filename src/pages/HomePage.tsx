7db922b4272ee60d2d513aec66726bd9866208ab
Sujal Shah <shahsujal2512@gmail.com>
2025-10-20 11:12:25 +0530

    Updated the code

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,40 +1,34 @@
-import { Center, Flex, Image, useMantineTheme } from "@mantine/core";
+import { Box, Center, Image, Stack } from "@mantine/core";
 import "../index.css";
 import { sections } from "./data.temp";
 
 const HomePage = () => {
-  const theme = useMantineTheme();
-
   return (
     <>
-      {sections.map((section) => (
-        <Flex
-          key={section.id}
-          id={section.id}
-          direction="column"
-          align="center"
-          justify="center"
-          pb="lg"
-          className="scrolltarget"
-          mx='auto'
-          w={{base:'100%',md:'50%'}}
-          
-        >
-          <Center w="100%">
-            <Image
-              src={section.img}
-              alt={section.id}
-              fit="fill"
-              radius="md"
-              style={{
-                boxShadow: "0 4px 12px rgba(0, 0, 0, 0.15)",
-                width: "100%",
-                borderRadius: "12px",
-              }}
-            />
-          </Center>
-        </Flex>
-      ))}
+      <Box w="100%" h="100%" bg="#02070e">
+        <Center w="100%" h="100%">
+          <Stack
+            justify="center"
+            align="center"
+            gap="md"
+            w={{ base: "100%", md: "50%" }}
+          >
+            {sections.map((section) => (
+              <Image
+                src={section.img}
+                alt={section.id}
+                fit="fill"
+                radius="md"
+                style={{
+                  boxShadow: "0 4px 12px rgba(0, 0, 0, 0.15)",
+                  width: "100%",
+                  borderRadius: "12px",
+                }}
+              />
+            ))}
+          </Stack>
+        </Center>
+      </Box>
     </>
   );
 };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,20 +2,16 @@ import { createBrowserRouter } from 'react-router-dom';
 
 import MasterLayout from '../layout/MasterLayout';
 import HomePage from '../pages/HomePage';
-import MainLayout from '../layout/MainLayout';
 
 export const routes = createBrowserRouter([
   {
     Component: MasterLayout,
     children: [
       {
-        Component : MainLayout,
-        children : [
-          {
+     
             index : true,
             Component : HomePage
-          }
-        ]
+        
       }
     ]
   }
